4ffca98cbd07d65b402ee999228ee9b7e1e15fa1
cldbci <claudiabaciu22@gmail.com>
2024-09-05 20:33:05 +0300

    commit project

---
+++ conn.php
@@ -0,0 +1,11 @@
+<?php 
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "quizapp";
+$conn = new PDO("mysql:host={$host};port=3306;dbname={$db};",$user,$pass);
+
+
+
+ ?>
---
+++ database/cee_db.sql
@@ -0,0 +1,332 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 05, 2020 at 04:19 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cee_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_acc`
+--
+
+CREATE TABLE `admin_acc` (
+  `admin_id` int(11) NOT NULL,
+  `admin_user` varchar(1000) NOT NULL,
+  `admin_pass` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin_acc`
+--
+
+INSERT INTO `admin_acc` (`admin_id`, `admin_user`, `admin_pass`) VALUES
+(1, 'admin@username', 'admin@password');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_tbl`
+--
+
+CREATE TABLE `course_tbl` (
+  `cou_id` int(11) NOT NULL,
+  `cou_name` varchar(1000) NOT NULL,
+  `cou_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `course_tbl`
+--
+
+INSERT INTO `course_tbl` (`cou_id`, `cou_name`, `cou_created`) VALUES
+(25, 'Informatica', '2024-08-26 10:00:00'),
+(26, 'Matematică', '2024-08-27 10:00:00'),
+(65, 'Istorie', '2024-08-28 10:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `examinee_tbl`
+--
+
+CREATE TABLE `examinee_tbl` (
+  `exmne_id` int(11) NOT NULL,
+  `exmne_fullname` varchar(1000) NOT NULL,
+  `exmne_course` varchar(1000) NOT NULL,
+  `exmne_gender` varchar(1000) NOT NULL,
+  `exmne_birthdate` varchar(1000) NOT NULL,
+  `exmne_year_level` varchar(1000) NOT NULL,
+  `exmne_email` varchar(1000) NOT NULL,
+  `exmne_password` varchar(1000) NOT NULL,
+  `exmne_status` varchar(1000) NOT NULL DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `examinee_tbl`
+--
+
+INSERT INTO `examinee_tbl` (`exmne_id`, `exmne_fullname`, `exmne_course`, `exmne_gender`, `exmne_birthdate`, `exmne_year_level`, `exmne_email`, `exmne_password`, `exmne_status`) VALUES
+(4, 'Ion Popescu', '26', 'male', '2000-01-15', 'anul 3', 'ion.popescu@gmail.com', 'ion123', 'active'),
+(5, 'Maria Ionescu', '25', 'female', '2001-02-20', 'anul 2', 'maria.ionescu@gmail.com', 'maria123', 'active'),
+(6, 'Andrei Georgescu', '26', 'male', '1999-11-10', 'anul 3', 'andrei.georgescu@gmail.com', 'andrei123', 'active'),
+(7, 'Elena Marin', '26', 'female', '2001-03-05', 'anul 2', 'elena.marin@gmail.com', 'elena123', 'active'),
+(8, 'Adrian Stoica', '26', 'male', '2000-12-21', 'anul 3', 'adrian.stoica@gmail.com', 'adrian123', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_answers`
+--
+
+CREATE TABLE `exam_answers` (
+  `exans_id` int(11) NOT NULL,
+  `axmne_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `quest_id` int(11) NOT NULL,
+  `exans_answer` varchar(1000) NOT NULL,
+  `exans_status` varchar(1000) NOT NULL DEFAULT 'new',
+  `exans_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `exam_answers`
+--
+
+INSERT INTO `exam_answers` (`exans_id`, `axmne_id`, `exam_id`, `quest_id`, `exans_answer`, `exans_status`, `exans_created`) VALUES
+(295, 4, 12, 25, 'Diode, inversat, indicator', 'old', '2024-08-29 12:00:00'),
+(296, 4, 12, 16, 'Bloc de date', 'old', '2024-08-29 12:10:00'),
+(297, 6, 12, 18, 'Controler Logic Programabil', 'old', '2024-08-29 12:20:00'),
+(298, 6, 12, 9, '1850', 'old', '2024-08-30 12:00:00'),
+(299, 6, 12, 24, '1976', 'old', '2024-08-30 12:10:00'),
+(300, 6, 12, 14, 'Sistem de operare', 'old', '2024-08-30 12:20:00'),
+(301, 6, 12, 19, 'WAN (Wide Area Network)', 'old', '2024-08-31 12:00:00'),
+(302, 6, 11, 28, 'fsd', 'new', '2024-08-31 12:10:00'),
+(303, 6, 11, 29, 'sd', 'new', '2024-08-31 12:20:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_attempt`
+--
+
+CREATE TABLE `exam_attempt` (
+  `examat_id` int(11) NOT NULL,
+  `exmne_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `examat_status` varchar(1000) NOT NULL DEFAULT 'used'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `exam_attempt`
+--
+
+INSERT INTO `exam_attempt` (`examat_id`, `exmne_id`, `exam_id`, `examat_status`) VALUES
+(51, 6, 12, 'used'),
+(52, 4, 11, 'used'),
+(53, 4, 12, 'used'),
+(54, 8, 12, 'used');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_question_tbl`
+--
+
+CREATE TABLE `exam_question_tbl` (
+  `eqt_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `exam_question` varchar(1000) NOT NULL,
+  `exam_ch1` varchar(1000) NOT NULL,
+  `exam_ch2` varchar(1000) NOT NULL,
+  `exam_ch3` varchar(1000) NOT NULL,
+  `exam_ch4` varchar(1000) NOT NULL,
+  `exam_answer` varchar(1000) NOT NULL,
+  `exam_status` varchar(1000) NOT NULL DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `exam_question_tbl`
+--
+
+INSERT INTO `exam_question_tbl` (`eqt_id`, `exam_id`, `exam_question`, `exam_ch1`, `exam_ch2`, `exam_ch3`, `exam_ch4`, `exam_answer`, `exam_status`) VALUES
+(9, 12, 'În ce deceniu a fost fondat Institutul American de Ingineri Electrotehnici (AIEE)?', '1850', '1880', '1930', '1950', '1880', 'active'),
+(10, 12, 'Ce parte a unei baze de date conține un singur tip de informație?', 'Raport', 'Câmp', 'Înregistrare', 'Fișier', 'Câmp', 'active'),
+(14, 12, 'Abrevierea OS în informatică se referă de obicei la?', 'Ordinea semnificației', 'Software deschis', 'Sistem de operare', 'Senzor optic', 'Sistem de operare', 'active'),
+(15, 12, 'Cine a dezvoltat Yahoo?', 'Dennis Ritchie & Ken Thompson', 'David Filo & Jerry Yang', 'Vint Cerf & Robert Kahn', 'Steve Case & Jeff Bezos', 'David Filo & Jerry Yang', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_tbl`
+--
+
+CREATE TABLE `exam_tbl` (
+  `ex_id` int(11) NOT NULL,
+  `cou_id` int(11) NOT NULL,
+  `ex_title` varchar(1000) NOT NULL,
+  `ex_time_limit` varchar(1000) NOT NULL,
+  `ex_questlimit_display` int(11) NOT NULL,
+  `ex_description` varchar(1000) NOT NULL,
+  `ex_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `exam_tbl`
+--
+
+INSERT INTO `exam_tbl` (`ex_id`, `cou_id`, `ex_title`, `ex_time_limit`, `ex_questlimit_display`, `ex_description`, `ex_created`) VALUES
+(11, 26, 'Examen Istorie', '30', 5, 'Examen rapid de istorie', '2024-09-01 10:00:00'),
+(12, 26, 'Examen Informatica', '45', 10, 'Examen complex de informatica', '2024-09-02 10:00:00'),
+(13, 26, 'Examen Matematică', '60', 15, 'Examen avansat de matematică', '2024-09-03 10:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedbacks_tbl`
+--
+
+CREATE TABLE `feedbacks_tbl` (
+  `fb_id` int(11) NOT NULL,
+  `exmne_id` int(11) NOT NULL,
+  `fb_exmne_as` varchar(1000) NOT NULL,
+  `fb_feedbacks` varchar(1000) NOT NULL,
+  `fb_date` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feedbacks_tbl`
+--
+
+INSERT INTO `feedbacks_tbl` (`fb_id`, `exmne_id`, `fb_exmne_as`, `fb_feedbacks`, `fb_date`) VALUES
+(4, 6, 'Andrei Georgescu', 'Aplicație foarte bine structurată', '2024-09-01'),
+(5, 6, 'Anonymous', 'Foarte utilă pentru examen!', '2024-09-01'),
+(6, 4, 'Ion Popescu', 'Îmi place cum sunt organizate chestionarele', '2024-09-02'),
+(9, 8, 'Anonymous', 'Aplicația funcționează bine', '2024-09-05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_acc`
+--
+ALTER TABLE `admin_acc`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `course_tbl`
+--
+ALTER TABLE `course_tbl`
+  ADD PRIMARY KEY (`cou_id`);
+
+--
+-- Indexes for table `examinee_tbl`
+--
+ALTER TABLE `examinee_tbl`
+  ADD PRIMARY KEY (`exmne_id`);
+
+--
+-- Indexes for table `exam_answers`
+--
+ALTER TABLE `exam_answers`
+  ADD PRIMARY KEY (`exans_id`);
+
+--
+-- Indexes for table `exam_attempt`
+--
+ALTER TABLE `exam_attempt`
+  ADD PRIMARY KEY (`examat_id`);
+
+--
+-- Indexes for table `exam_question_tbl`
+--
+ALTER TABLE `exam_question_tbl`
+  ADD PRIMARY KEY (`eqt_id`);
+
+--
+-- Indexes for table `exam_tbl`
+--
+ALTER TABLE `exam_tbl`
+  ADD PRIMARY KEY (`ex_id`);
+
+--
+-- Indexes for table `feedbacks_tbl`
+--
+ALTER TABLE `feedbacks_tbl`
+  ADD PRIMARY KEY (`fb_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_acc`
+--
+ALTER TABLE `admin_acc`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `course_tbl`
+--
+ALTER TABLE `course_tbl`
+  MODIFY `cou_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+
+--
+-- AUTO_INCREMENT for table `examinee_tbl`
+--
+ALTER TABLE `examinee_tbl`
+  MODIFY `exmne_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `exam_answers`
+--
+ALTER TABLE `exam_answers`
+  MODIFY `exans_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=321;
+
+--
+-- AUTO_INCREMENT for table `exam_attempt`
+--
+ALTER TABLE `exam_attempt`
+  MODIFY `examat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+
+--
+-- AUTO_INCREMENT for table `exam_question_tbl`
+--
+ALTER TABLE `exam_question_tbl`
+  MODIFY `eqt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `exam_tbl`
+--
+ALTER TABLE `exam_tbl`
+  MODIFY `ex_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `feedbacks_tbl`
+--
+ALTER TABLE `feedbacks_tbl`
+  MODIFY `fb_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
