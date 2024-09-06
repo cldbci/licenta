7a50626aecbc3f78b4929166647cba86bea83962
cldbci <84238751+cldbci@users.noreply.github.com>
2024-09-06 09:25:45 +0300

    Update cee_db.sql

--- database/cee_db.sql
+++ database/cee_db.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 05, 2020 at 04:19 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
